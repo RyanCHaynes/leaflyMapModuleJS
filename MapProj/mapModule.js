1518522a943540ba1238d2fa9aac27e1cecaacea
Charles Ryan Haynes <ryanchaynes@hotmail.com>
2018-09-14 09:23:10 -0500

    MapModBeta.01

---
+++ MapProj/mapModule.js
@@ -0,0 +1,4 @@
+     var mymap = L.map('mapid').setView([42.35, -71.08], 13);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
